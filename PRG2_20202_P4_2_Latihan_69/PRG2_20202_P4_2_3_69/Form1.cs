95b2fc43fc2fca1df0b44c528864b4b458b40129
SALMAN AL ARIFIN <0320210069@polman.astra.ac.id>
2022-05-31 17:57:39 +0700

    SALMAN

---
+++ PRG2_20202_P4_2_Latihan_69/PRG2_20202_P4_2_3_69/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG2_20202_P4_2_3_69
+{
+    public partial class TBForm : Form
+    {
+        public TBForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
